27462e747c3f3e561409adbc25fc13378f637fff
bmoryson <65439125+bmoryson@users.noreply.github.com>
2020-05-16 09:02:37 +0200

    Create wykladowca.java

---
+++ wykladowca.java
@@ -0,0 +1,37 @@
+package pl.bmoryson;
+class Wykladowca extends Czlowiek {
+  private String tytul_naukowy;
+  private String przedmiot;
+
+  Wykladowca (){
+   System.out.println("wywolano konstruktor klasy wykladowca"); 
+  }
+
+  public String getPrzedmiot() {
+    return przedmiot;
+  }
+
+  public void setPrzedmiot(String przedmiot) {
+    this.przedmiot = przedmiot;
+  }
+
+  public String getTytul_naukowy() {
+    return nazwisko;
+  }
+
+  public void setTytul_naukowy(String tytul_naukowy) {
+    this.tytul_naukowy = tytul_naukowy;
+  }
+
+
+  @Override
+  public String toString() {
+    return "Wykładowca{" +
+                "imie='" + imie + '\'' +
+                ", nazwisko='" + nazwisko + '\'' +
+                ", PESEL='" + PESEL + '\'' +            
+                ", tytul_naukowy='" + tytul_naukowy + '\'' +
+                ", przedmiot='" + przedmiot + '\'' +
+               '}';
+  }
+}
