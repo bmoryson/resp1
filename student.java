d1d20046064209c1aa6409793f19aa79d5fbf1f0
bmoryson <65439125+bmoryson@users.noreply.github.com>
2020-05-16 09:01:40 +0200

    Create student.java

---
+++ student.java
@@ -0,0 +1,45 @@
+package pl.bmoryson;
+class Student extends Czlowiek implements Comparable<Student> {
+  private long index;
+  private double srednia;
+
+  Student() {
+    System.out.println("wywolano konstruktor klasy student");
+  }
+
+  public long getIndex() {
+    return index;
+  }
+
+  public void setIndex(long index) {
+    this.index = index;
+  }
+  public double getSrednia() {
+    return srednia;
+  }
+
+  public void setSrednia(double srednia) {
+    this.srednia = srednia;
+  }
+
+  @Override
+  public String toString() {
+    return "Student{" +
+                "imie='" + imie + '\'' +
+                ", nazwisko='" + nazwisko + '\'' +
+                ", PESEL='" + PESEL + '\'' +
+                "index='" + index + '\'' +
+                ", srednia='" + srednia + '\'' +
+                '}';
+  }
+
+  @Override
+  public int compareTo(Student student) {
+    if (student.getIndex() == index)
+      return 0;
+    else if (student.getIndex() < index)
+      return 1;
+    else
+      return -1; 
+  }
+}
