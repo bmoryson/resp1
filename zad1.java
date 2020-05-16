05aaf4f06fb95c1615cb54e80f42c0fa7fa0aff1
bmoryson <65439125+bmoryson@users.noreply.github.com>
2020-05-16 09:22:21 +0200

    Update and rename zad1 to zad1.java

---
+++ zad1.java
@@ -0,0 +1,26 @@
+package pl.bmoryson;
+class zad1 {
+    public static void main(String[] args) {
+    
+    Czlowiek o1 = new Czlowiek ();
+    o1.setImie("Andrzej");
+    o1.setNazwisko("Kowalski");
+    o1.setPESEL("1212121212");
+    
+    Student student = new Student ();
+    student.setImie("Adam");
+    student.setNazwisko("Testowicz");
+    student.setPESEL("1313131313");
+    student.setIndex(989898L);
+    student.setSrednia(4.0);
+    
+    Wykladowca wykladowca = new Wykladowca ();
+    wykladowca.setImie("Michał");
+    wykladowca.setNazwisko("Michałowicz");
+    wykladowca.setPESEL("1414141414");
+    wykladowca.setTytul_naukowy("Magister");
+    wykladowca.setPrzedmiot("Matematyka");
+
+    System.out.println(wykladowca);
+  }
+}
